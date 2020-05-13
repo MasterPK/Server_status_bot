ef0b01b31838e27dda7f8ae6b9695a26f7473568
Petr Křehlík <xkrehl04@stud.fit.vutbr.cz>
2020-05-13 20:09:10 +0200

    Beta 13052020

--- src/main/java/bots/ServerStatus.java
+++ src/main/java/bots/ServerStatus.java
@@ -2,11 +2,14 @@ package bots;
 
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.entities.GuildChannel;
+import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.events.ReadyEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.exceptions.ContextException;
+import net.dv8tion.jda.api.exceptions.InsufficientPermissionException;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.requests.restaction.MessageAction;
 
 import javax.annotation.Nonnull;
 import javax.xml.soap.Text;
@@ -19,28 +22,47 @@ import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
+/**
+ * Class that showing server status every minute.
+ * @author Petr Křehlík
+ */
 public class ServerStatus extends ListenerAdapter {
-    private boolean status = false;
+    /**
+     * API object.
+     */
     private JDA api;
 
-
+    /**
+     * Show immediately server status everywhere on server if user write "!server-status"
+     * @param event Event from API.
+     */
     @Override
     public void onMessageReceived(@Nonnull MessageReceivedEvent event) {
         if (event.getMessage().getContentRaw().equals("!server-status")) {
-            event.getChannel().sendMessage("Online").queue();
+            boolean status = pingHost("178.63.23.23", 28061, 5000);
+            String test = "Aktualni stav serveru: " + (status ? "Online" : "Offline");
+            event.getChannel().sendMessage(test).queue();
         }
     }
 
+    /**
+     * Initialize object.
+     * When API is ready starts 1 minute timer to update data.
+     * @param event
+     */
     @Override
     public void onReady(@Nonnull ReadyEvent event) {
-        this.api=event.getJDA();
-        Timer timer = new Timer("30 sec");
+        this.api = event.getJDA();
+        Timer timer = new Timer("1");
         timer.schedule(updateTask, 0, 60000);
     }
 
+    /**
+     * Update status in channel "server-status".
+     */
     private void updateStatus() {
 
-        this.status = pingHost("178.63.23.23", 28061, 5000);
+        boolean status = pingHost("178.63.23.23", 28061, 5000);
         List<TextChannel> textChannels = api.getTextChannelsByName("server-status", true);
 
         if (textChannels.isEmpty()) {
@@ -56,16 +78,29 @@ public class ServerStatus extends ListenerAdapter {
             return;
         }
 
+        String messageId = textChannel.getLatestMessageId();
+        Message message = textChannel.getHistory().getMessageById(messageId);
 
+        String test = "Aktualni stav serveru: " + (status ? "Online" : "Offline");
 
-        String test = new String("Aktualni stav serveru: " + (this.status ? "Online" : "Offline"));
+        if(message==null)
+        {
+            textChannel.sendMessage(test).queue();
+            return;
+        }
 
-        test=new String(test.getBytes(StandardCharsets.UTF_8), StandardCharsets.UTF_8);
-        //textChannel.sendMessage(test).queue();
-
-        textChannel.editMessageById(lastMessageId, test).queue();
+        if(message.getContentDisplay().contains("Aktualni"))
+        {
+            MessageAction messageAction = textChannel.editMessageById(lastMessageId, test);
+            messageAction.queue();
+        }else {
+            textChannel.sendMessage(test).queue();
+        }
     }
 
+    /**
+     * Timer task. Run update of status.
+     */
     private TimerTask updateTask = new TimerTask() {
         @Override
         public void run() {
@@ -73,6 +108,13 @@ public class ServerStatus extends ListenerAdapter {
         }
     };
 
+    /**
+     * Ping IP and port.
+     * @param host IP to ping.
+     * @param port Port to ping.
+     * @param timeout Timeout in ms.
+     * @return Return true if ping succeed. False if no response in timeout.
+     */
     private boolean pingHost(String host, int port, int timeout) {
         try (Socket socket = new Socket()) {
             socket.connect(new InetSocketAddress(host, port), timeout);
